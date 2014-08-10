60e36a7933f2f518c74bb8ee8bacc595b8e05460
Stig Brautaset <stig@brautaset.org>
2014-08-10 18:19:33 +0100

    "Downgrade" to melpa-stable repository

--- setup/setup-package.el
+++ setup/setup-package.el
@@ -2,8 +2,8 @@
 ; (require 'dash)
 
 ;; Add melpa to package repos
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 (package-initialize)
 
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
