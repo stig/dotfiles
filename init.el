b8fa264d06c2efc86d6c132e7f58414aa8fe5348
Stig Brautaset <stig@brautaset.org>
2014-08-20 22:54:04 +0100

    Move clojure refactoring back to C-c C-m

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
 ;; Turn on Clojure refactoring minor mode
 (add-hook 'clojure-mode-hook (lambda ()                             
                                (clj-refactor-mode 1)
-                               (cljr-add-keybindings-with-prefix "C-c C-c")
+                               (cljr-add-keybindings-with-prefix "C-c C-m")
                                (define-key clj-refactor-map (kbd "C-x C-r") 'cljr-rename-file)
                                (define-key clojure-mode-map (kbd "s-j") 'clj-jump-to-other-file)))
 
