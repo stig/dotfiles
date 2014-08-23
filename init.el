afaf954fc361bc780ec9078ca122285e3f02447a
Stig Brautaset <stig@brautaset.org>
2014-08-23 22:27:03 +0100

    Better keys for multiple-cursors package

--- init.el
+++ init.el
@@ -68,9 +68,9 @@
 
 ;; Multiple cursors...
 (require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-s-c C-s-c") 'mc/edit-lines)
+(global-set-key (kbd "s-<right>") 'mc/mark-next-like-this)
+(global-set-key (kbd "s-<left>") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-a") 'mc/mark-all-like-this)
 
 ;; Windsize
