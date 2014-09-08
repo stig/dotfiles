cf5fd9ef0700f710f583e92a4a78c49c7c971354
Stig Brautaset <stig.brautaset@net-a-porter.com>
2014-09-08 11:01:30 +0100

    Press s-c to comment/uncomment region or line

--- init.el
+++ init.el
@@ -101,6 +101,18 @@
 ;; Allow ssh+sudo with tramp
 (set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
 
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)
+        (next-line)))
+
+(global-set-key (kbd "s-c") 'comment-or-uncomment-region-or-line)
+
 ;; Emacs server
 (require 'server)
 (unless (server-running-p)
