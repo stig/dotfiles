6fb642e086f1155dc4bd2f41c95d02568a8ca7ee
Stig Brautaset <stig.brautaset@net-a-porter.com>
2014-09-10 22:16:05 +0100

    Use when rather than if (since there is no else)

--- init.el
+++ init.el
@@ -1,12 +1,13 @@
 ; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
 
 (add-to-list 'load-path "~/.emacs.d/setup")
+(require 'setup-defaults)
 (require 'setup-package)
 
 ;; Install extensions if they're missing
@@ -30,7 +31,6 @@
    (package-refresh-contents)
    (init--install-packages)))
 
-(require 'setup-defaults)
 (require 'setup-mail)
 
 ;; Load a nice theme...
