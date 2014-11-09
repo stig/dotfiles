904beef3f88f9239fb93e896be3b92d92c87c74d
Stig Brautaset <stig@brautaset.org>
2014-11-09 22:37:30 +0000

    Double ;; for comments

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-; Turn off mouse interface early in startup to avoid momentary display
+;; Turn off mouse interface early in startup to avoid momentary display
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
