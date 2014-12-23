c3b4822b4b681dc1b36f026b528d610bfbfc24e2
Stig Brautaset <stig.brautaset@net-a-porter.com>
2014-12-23 12:50:23 +0000

    Add Scala mode

--- init.el
+++ init.el
@@ -332,6 +332,7 @@
 (package-install? 'multiple-cursors)
 (require 'multiple-cursors)
 (global-set-key (kbd "C-c a") 'mc/edit-lines)
+(global-set-key (kbd "C-c C-a") 'mc/mark-all-dwim)
 (global-set-key (kbd "s-n") 'mc/mark-next-like-this)
 (global-set-key (kbd "s-p") 'mc/mark-previous-like-this)
 
@@ -391,6 +392,31 @@
     (insert (format-time-string "%Y-%m-%d")))
 
 (global-set-key (kbd "C-x M-d") 'insert-current-date)
+
+(package-install? 'scala-mode2)
+(add-hook 'scala-mode-hook '(lambda ()
+   ;; sbt-find-definitions is a command that tries to find (with grep)
+   ;; the definition of the thing at point.
+   (local-set-key (kbd "M-.") 'sbt-find-definitions)
+
+   ;; use sbt-run-previous-command to re-compile your code after changes
+   (local-set-key (kbd "C-x '") 'sbt-run-previous-command)))
+
+(package-install? 'sbt-mode)
+(add-hook 'sbt-mode-hook '(lambda ()
+  ;; compilation-skip-threshold tells the compilation minor-mode
+  ;; which type of compiler output can be skipped. 1 = skip info
+  ;; 2 = skip info and warnings.
+  (setq compilation-skip-threshold 1)
+
+  ;; Bind C-a to 'comint-bol when in sbt-mode. This will move the
+  ;; cursor to just after prompt.
+  (local-set-key (kbd "C-a") 'comint-bol)
+
+  ;; Bind M-RET to 'comint-accumulate. This will allow you to add
+  ;; more than one line to scala console prompt before sending it
+  ;; for interpretation. It will keep your command history cleaner.
+  (local-set-key (kbd "M-RET") 'comint-accumulate)))
 
 (package-install? 'sane-term)
 (global-set-key (kbd "C-x t") 'sane-term)
