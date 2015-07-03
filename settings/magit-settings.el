35e50daa479b18799a6e84746e432372f2175681
Stig Brautaset <stig@brautaset.org>
2015-07-03 23:10:15 +0100

    Enable magit-gh-pulls

--- settings/magit-settings.el
+++ settings/magit-settings.el
@@ -3,8 +3,8 @@
 
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 
-;; (require 'magit-gh-pulls)
-;; (add-hook 'magit-mode-hook 'magit-gh-pulls-mode)
+(require 'magit-gh-pulls)
+(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
 
 (setq magit-git-executable "/usr/bin/git")
 
