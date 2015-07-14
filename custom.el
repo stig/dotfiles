b5c5bce7e8c8d7b1c447656b307377d3846f082e
Stig Brautaset <stig@brautaset.org>
2015-07-14 11:06:00 +0100

    Always prune when fetching

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-fetch-arguments (quote ("--prune")))
  '(paradox-automatically-star t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
