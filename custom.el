170fd21a58da8a03c0d909d186678028dce230cb
Stig Brautaset <stig@brautaset.org>
2015-07-28 23:36:54 +0200

    Don't set upstream branch name on branch

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-branch-arguments nil)
  '(magit-fetch-arguments (quote ("--prune")))
  '(paradox-automatically-star t))
 (custom-set-faces
