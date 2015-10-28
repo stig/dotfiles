c6a13f9a4e1a115471c934f65235009b443dbe9b
Stig Brautaset <stig@brautaset.org>
2015-10-28 18:54:28 +0000

    Support site-lisp packages installed by homebrew

--- init.el
+++ init.el
@@ -13,6 +13,9 @@
 (setq secret-file (expand-file-name "secret.el" user-emacs-directory))
 (load secret-file)
 
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
 ;; Fill at 78 columns rather than the default of 70
 (setq-default fill-column 78)
 
