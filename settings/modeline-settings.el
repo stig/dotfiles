5c8f0cb6c796f6bad9deae52575af65d5a38bc05
Stig Brautaset <stig@brautaset.org>
2015-10-29 12:22:27 +0000

    I'll stick with default modelines for now

--- settings/modeline-settings.el
+++
@@ -1,9 +0,0 @@
-(defmacro rename-modeline (package-name mode new-name)
-  `(eval-after-load ,package-name
-     '(defadvice ,mode (after rename-modeline activate)
-        (setq mode-name ,new-name))))
-
-(rename-modeline "js2-mode" js2-mode "JS2")
-(rename-modeline "clojure-mode" clojure-mode "Clj")
-
-(provide 'modeline-settings)
