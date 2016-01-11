0aee8e9813b421d8e602fe5a4d86904b34c6cee6
Stig Brautaset <sbrautaset@laterpay.net>
2016-01-11 15:32:28 +0000

    Remove cargo-cult stuff related to tramp

--- init.el
+++ init.el
@@ -205,18 +205,9 @@
 (unless (server-running-p)
   (server-start))
 
-;; Fix to allow editing remote files over ssh
-(put 'temporary-file-directory
-     'standard-value
-     '((file-name-as-directory "/tmp")))
-
 ;; Allow ssh+sudo with tramp
 (set-default 'tramp-default-proxies-alist
              (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
-
-;; SSH is faster than the default scp mode (apparently)
-(setq tramp-default-method "ssh")
-
 
 ;;
 ;; Packages installed with package.el
