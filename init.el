46dd2626bf0d6671701da0bba12f62f300758cb1
Stig Brautaset <stig@brautaset.org>
2018-05-07 17:25:18 +0100

    It appears loading package.el is optional now

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-(require 'package)
-
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
