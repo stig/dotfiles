b88b37523f9a8c53ce42a65c3326126ecfc1f776
Stig Brautaset <stig@brautaset.org>
2021-04-18 00:02:55 +0100

    setup: install nix if necessary at setup-time

--- setup.sh
+++ setup.sh
@@ -14,3 +14,8 @@ mkdir -p ~/.nixpkgs
 ln -vsf $(pwd)/nixpkgs/* ~/.nixpkgs
 
 which brew || ./install-brew.sh
+which nix-env || ./install-nix.sh --darwin-use-unencrypted-nix-store-volume
+which darwin-rebuild || {
+    nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+    (echo n ; yes) | ./result/bin/darwin-installer
+}
