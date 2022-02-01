af893eaa3a20876f54bae19d02205a915a386da7
Stig Brautaset <stig@brautaset.org>
2022-02-01 23:05:12 +0000

    Bump install-brew.sh and install-nix.sh

--- install-nix.sh
+++ install-nix.sh
@@ -25,27 +25,72 @@ require_util() {
 }
 
 case "$(uname -s).$(uname -m)" in
-    Linux.x86_64) system=x86_64-linux; hash=2ea0cd17d53b2e860ec8e17b6de578aff1b11ebaf57117714a250bfd02768834;;
-    Linux.i?86) system=i686-linux; hash=8f352160dad90847d5a76f36ceb6d64b9cb92a113ece62fbdf523ceda3dd08b1;;
-    Linux.aarch64) system=aarch64-linux; hash=cc61f06d614586350963c010d0bd939f2c9f898a813940ca61148d385982fec0;;
-    Darwin.x86_64) system=x86_64-darwin; hash=9ea2f3c0d5de42ea5646864af72fe4d7bb7379cb98f771315f0c8dc86fb6dc8d;;
-    # eventually maybe: system=arm64-darwin; hash=@binaryTarball_arm64-darwin@;;
-    Darwin.arm64) system=x86_64-darwin; hash=9ea2f3c0d5de42ea5646864af72fe4d7bb7379cb98f771315f0c8dc86fb6dc8d;;
+    Linux.x86_64)
+        hash=766246f85c68de879856a96c6b41fde2a94452ae030583da59fca9a982691de8
+        path=qlpjzrbphrm2yq215lszwwkqqv77g0wc/nix-2.6.0-x86_64-linux.tar.xz
+        system=x86_64-linux
+        ;;
+    Linux.i?86)
+        hash=a782589ee5ae028b9f281d157e5a4191a322ec2f9ef034088d04a8e6b956aef1
+        path=zgs26gkky99mc7ffifdgyjclnpcjh6p7/nix-2.6.0-i686-linux.tar.xz
+        system=i686-linux
+        ;;
+    Linux.aarch64)
+        hash=b7d05160f25a05ddf4a66b5427d160de71ca741cc12ef39b50c0bbdac184a4cb
+        path=k7n53gs1jy0zgnahd56fs1nxcbp2wykx/nix-2.6.0-aarch64-linux.tar.xz
+        system=aarch64-linux
+        ;;
+    Linux.armv6l_linux)
+        hash=5e0675bceb91ae1ed5590ce500b62af2341367d1c00b2a650ce56ec6fe97ce00
+        path=r7p3v8j4m216aw1a2cv8iqbnpb0z48yp/nix-2.6.0-armv6l-linux.tar.xz
+        system=armv6l-linux
+        ;;
+    Linux.armv7l_linux)
+        hash=aee888c395da0e6b305df712cbbdafa525ddbd18808cecc25dbdc4431ee41509
+        path=2yi90ll2z6if0nqqg71ihzzwx6nlsdq6/nix-2.6.0-armv7l-linux.tar.xz
+        system=armv7l-linux
+        ;;
+    Darwin.x86_64)
+        hash=9e38fbc023e7de18775dd71a0ac29854ca7a6a33c3a0bfa840a30bb5693c60da
+        path=a7d65zp98030ndg76cp840mgx6nn4yp9/nix-2.6.0-x86_64-darwin.tar.xz
+        system=x86_64-darwin
+        ;;
+    Darwin.arm64|Darwin.aarch64)
+        hash=f21e7a3547923fc47c581dd02583af47985d8377ab80744c0ed5df0aea289ad2
+        path=xf89mxrwq82gj7k8ncvb7z7faxv5ya6w/nix-2.6.0-aarch64-darwin.tar.xz
+        system=aarch64-darwin
+        ;;
     *) oops "sorry, there is no binary distribution of Nix for your platform";;
 esac
 
-url="https://releases.nixos.org/nix/nix-2.3.10/nix-2.3.10-$system.tar.xz"
+# Use this command-line option to fetch the tarballs using nar-serve or Cachix
+if [ "${1:-}" = "--tarball-url-prefix" ]; then
+    if [ -z "${2:-}" ]; then
+        oops "missing argument for --tarball-url-prefix"
+    fi
+    url=${2}/${path}
+    shift 2
+else
+    url=https://releases.nixos.org/nix/nix-2.6.0/nix-2.6.0-$system.tar.xz
+fi
 
-tarball="$tmpDir/$(basename "$tmpDir/nix-2.3.10-$system.tar.xz")"
+tarball=$tmpDir/nix-2.6.0-$system.tar.xz
 
-require_util curl "download the binary tarball"
 require_util tar "unpack the binary tarball"
 if [ "$(uname -s)" != "Darwin" ]; then
     require_util xz "unpack the binary tarball"
 fi
 
-echo "downloading Nix 2.3.10 binary tarball for $system from '$url' to '$tmpDir'..."
-curl -L "$url" -o "$tarball" || oops "failed to download '$url'"
+if command -v curl > /dev/null 2>&1; then
+    fetch() { curl -L "$1" -o "$2"; }
+elif command -v wget > /dev/null 2>&1; then
+    fetch() { wget "$1" -O "$2"; }
+else
+    oops "you don't have wget or curl installed, which I need to download the binary tarball"
+fi
+
+echo "downloading Nix 2.6.0 binary tarball for $system from '$url' to '$tmpDir'..."
+fetch "$url" "$tarball" || oops "failed to download '$url'"
 
 if command -v sha256sum > /dev/null 2>&1; then
     hash2="$(sha256sum -b "$tarball" | cut -c1-64)"
@@ -68,6 +113,7 @@ tar -xJf "$tarball" -C "$unpack" || oops "failed to unpack '$url'"
 script=$(echo "$unpack"/*/install)
 
 [ -e "$script" ] || oops "installation script is missing from the binary tarball!"
+export INVOKED_FROM_INSTALL_IN=1
 "$script" "$@"
 
 } # End of wrapping
