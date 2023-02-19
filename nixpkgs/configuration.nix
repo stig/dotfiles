0fbe9711894637a4eaecf0a2a9151321e99b1b23
Stig Brautaset <stig@brautaset.org>
2023-02-19 15:55:49 +0000

    nix: rename darwin-configuration.nix to configuration.nix

--- nixpkgs/configuration.nix
+++ nixpkgs/configuration.nix
@@ -105,7 +105,7 @@
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+  environment.darwinConfig = "$HOME/.config/nixpkgs/configuration.nix";
 
   users.users.stig = {
     name = "stig";
