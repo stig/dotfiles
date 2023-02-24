44d5d80cd2569e42f0c18436490e4e4a187bfe23
Stig Brautaset <stig@brautaset.org>
2023-02-24 10:18:12 +0000

    lein: add the deps-plus plugin to its own profile

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,4 +1,5 @@
 {:user {:pedantic? :warn}
+ :deps-plus {:plugins [[com.circleci/deps-plus "0.1.0-SNAPSHOT"]]}
  :repl {:dependencies [[nrepl/nrepl "RELEASE"]]
         :plugins [[refactor-nrepl/refactor-nrepl "RELEASE"]
                   [cider/cider-nrepl "RELEASE"]
