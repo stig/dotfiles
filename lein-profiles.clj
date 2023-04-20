440473b15fbf1d0f4952d18fd61fe52e1894a136
Stig Brautaset <stig@brautaset.org>
2023-04-20 12:24:59 +0100

    lein: Remove :repl from profile as CIDER will inject them

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,6 +1,2 @@
 {:user {:pedantic? :warn}
- :deps-plus {:plugins [[com.circleci/deps-plus "0.1.0-SNAPSHOT"]]}
- :repl {:dependencies [[nrepl/nrepl "RELEASE"]]
-        :plugins [[refactor-nrepl/refactor-nrepl "RELEASE"]
-                  [cider/cider-nrepl "RELEASE"]
-                  [mx.cider/enrich-classpath "RELEASE"]]}}
+ :deps-plus {:plugins [[com.circleci/deps-plus "0.1.0-SNAPSHOT"]]}}
